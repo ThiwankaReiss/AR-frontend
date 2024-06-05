2951b0a5175528ef74efbd6ae92fec3cb20bbaa6
ThiwankaReiss <thiwankar2003@gmail.com>
2024-06-06 01:19:36 +0530

    Completed checkout and initialised orders page

--- src/pages/Checkout/Checkout.jsx
+++ src/pages/Checkout/Checkout.jsx
@@ -2,12 +2,14 @@ import React, { useEffect, useState } from 'react'
 import state from '../../store'
 import { useSnapshot } from 'valtio'
 import { useForm } from 'react-hook-form'
+import axios from 'axios'
+import Swal from 'sweetalert2'
 const Checkout = () => {
 
     const snap = useSnapshot(state);
     const { handleSubmit, register, reset, formState: { errors } } = useForm();
     const [selectedItm, setSelectedItm] = useState(null);
-    
+    const [total, setTotal] = useState(0.00);
 
     const handleDelete = () => {
         const newItems = snap.orderDetail.filter((item, i) => i !== selectedItm);
@@ -16,14 +18,61 @@ const Checkout = () => {
     const submit = async (data) => {
         const newArray = [...snap.orderDetail];
         newArray[selectedItm] = {
-          ...newArray[selectedItm],
-          amount: data.amount
+            ...newArray[selectedItm],
+            amount: data.amount
         };
         state.orderDetail = newArray;
     }
- 
+    const handlePlaceOrder=()=>{
+        Swal.fire('Please wait')
+        Swal.showLoading();
+        axios.post('http://localhost:8080/order', {
+
+        user:snap.customer,
+        total :total,
+        detail:snap.orderDetail
+        })
+            .then(function (response) {
+
+
+                if (response.data != null && response.data != '') {
+
+                    console.log(response.data)
+                    Swal.fire({
+                        title: "Sucess!",
+                        text: "Registration Sucessfully!",
+                        icon: "success"
+                    });
+                    Swal.hideLoading();
+
+                    state.orderDetail=[];
+
+                } else {
+                    Swal.fire({
+                        icon: "error",
+                        title: "Failed!",
+                        text: "Something went wrong",
+                    });
+                    Swal.hideLoading();
+                }
+            })
+            .catch(function (error) {
+
+            });
+    }
+    useEffect(() => {
+        var tot = 0;
+        snap.orderDetail.forEach(element => {
+            tot += element.price * element.amount;
+        });
+        setTotal(tot);
+    }, [snap.orderDetail])
+
     return (
         <div className="container">
+            <div className="row mt-3 text-end">
+                <h3>Total : Rs {total}/=</h3>
+            </div>
             <div className="row">
 
                 <table class="table text-center table-bordered mt-3">
@@ -57,7 +106,21 @@ const Checkout = () => {
 
                     </tbody>
                 </table>
+                <div className='col-12 d-flex justify-content-end'>
+                    <button
+                        data-bs-toggle="modal"
+                        data-bs-target="#exampleModal3"
+                        style={{
+                            backgroundColor: snap.themeColor, padding: "5px",
+                            borderRadius: "10px", color: "white"
+                        }}
+                    >
+                        Place Order
+                    </button>
+                </div>
+
             </div>
+
 
 
             {/* modal 1 */}
@@ -93,7 +156,7 @@ const Checkout = () => {
                                 class="form-control w-50"
                                 id="inputPassword"
                                 placeholder="amount"
-                               
+
                                 {...register("amount", { required: true, pattern: /^(?!0+$)\d+$/ })}
                             ></input>
                             {errors && errors.amount && (<p>Enter valid amount</p>)}
@@ -102,7 +165,25 @@ const Checkout = () => {
                             <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">Close</button>
                             <button type="button" class="btn btn-primary" data-bs-dismiss={!errors.amount && 'modal'} onClick={handleSubmit(submit)}>Confirm</button>
                         </div>
-                        
+
+                    </div>
+                </div>
+            </div>
+             {/* modal31 */}
+             <div class="modal fade" id="exampleModal3" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h1 class="modal-title fs-5" id="exampleModalLabel">Confirm Order</h1>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            Please Confirm Your Order
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-primary" data-bs-dismiss="modal" onClick={handlePlaceOrder}>Confim</button>
+                        </div>
                     </div>
                 </div>
             </div>
